8d7a30454b7fdeea472b06da0bf528fb4439ed22
blesy123 <81622538+blesy123@users.noreply.github.com>
2021-04-14 13:32:10 +0530

    Add files via upload

---
+++ d4/Engine.java
@@ -0,0 +1,44 @@
+package d4;
+
+public class Engine {
+private int basepower;
+	
+	public Engine() {
+		super();
+	}
+
+	public Engine(int basepower) {
+		this.basepower = basepower;
+	}
+	
+	public int getBasepower() {
+		return basepower;
+	}
+
+	public void setBasepower(int basepower) {
+		this.basepower = basepower;
+	}
+
+	public int doStartEngine(String fuelType)
+	{
+		// --- 1000 lines of code to generate power;
+		if(fuelType.equals("petrol"))
+		{
+			return basepower*100;
+		}
+		else if(fuelType.equals("diesel"))
+		{
+			return basepower*250;
+		}
+		else if(fuelType.equals("cng"))
+		{
+			return basepower*50;
+		}
+		else 
+		{
+			return basepower*90;
+		}
+		
+	}
+
+}
